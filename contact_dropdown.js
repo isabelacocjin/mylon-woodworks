a4907359647848cb530985bee283f3f6630b9ef2
isabelacocjin <itcocjin0329ant@student.fatima.edu.ph>
2026-04-29 16:36:02 +0800

    Updated website with better responsiveness.
    Web Development Task: Responsive Website

--- contact_dropdown.js
+++ contact_dropdown.js
@@ -1,26 +1,24 @@
-/*"other" option in dropdown; there's smt wrong*/
-
-
 document.addEventListener("DOMContentLoaded", function () {
 
   const projectSelect = document.getElementById("projectSelect");
   const messageBox = document.getElementById("otherMessage");
 
-  // Safety check
+
   if (!projectSelect || !messageBox) {
     console.error("Dropdown or textarea not found. Check your IDs.");
     return;
   }
 
-  // Hide textarea by default (in case CSS fails)
   messageBox.style.display = "none";
 
-  // Listen for dropdown change
+
   projectSelect.addEventListener("change", function () {
     if (this.value === "other") {
       messageBox.style.display = "block";
+      messageBox.value = "";
     } else {
       messageBox.style.display = "none";
+      messageBox.value = "";
     }
   });
 
