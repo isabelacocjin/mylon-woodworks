0c40b856be29cea28f96bcc8033ca267e040b348
isabelacocjin <itcocjin0329ant@student.fatima.edu.ph>
2026-05-05 11:40:27 +0800

    Small changes and updates
    Contact form, gallery and about page to be edited/polished

--- contact_dropdown.js
+++ contact_dropdown.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-  const projectSelect = document.getElementById("projectSelect");
-  const messageBox = document.getElementById("otherMessage");
-
-
-  if (!projectSelect || !messageBox) {
-    console.error("Dropdown or textarea not found. Check your IDs.");
-    return;
-  }
-
-  messageBox.style.display = "none";
+//   const projectSelect = document.getElementById("projectSelect");
+//   const messageBox = document.getElementById("otherMessage");
 
 
-  projectSelect.addEventListener("change", function () {
-    if (this.value === "other") {
-      messageBox.style.display = "block";
-      messageBox.value = "";
-    } else {
-      messageBox.style.display = "none";
-      messageBox.value = "";
-    }
-  });
+//   if (!projectSelect || !messageBox) {
+//     console.error("Dropdown or textarea not found. Check your IDs.");
+//     return;
+//   }
 
-});
+//   messageBox.style.display = "none";
+
+
+//   projectSelect.addEventListener("change", function () {
+//     if (this.value === "other") {
+//       messageBox.style.display = "block";
+//       messageBox.value = "";
+//     } else {
+//       messageBox.style.display = "none";
+//       messageBox.value = "";
+//     }
+//   });
+
+// });
